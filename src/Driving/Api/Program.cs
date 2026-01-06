f596dc2abc67de8c48ba7932f9b69d8388022839
PauloBusch <paulo202015@outlook.com.br>
2026-01-06 20:19:01 +0000

    Update Swagger UI and add OpenAPI enhancements
    - Updated Swagger UI configuration:
      - Changed API title to "FCG Catalogs API".
      - Added OAuth support with PKCE, client ID, and app name.
      - Removed `RoutePrefix` to serve Swagger UI at the root URL.
    - Added `MapOpenApi` to the application pipeline for OpenAPI endpoint mapping.
    - Explicitly added `OpenSearchHealthCheck` with the "search" tag.
    - Improved development experience and API usability.

--- src/Driving/Api/Program.cs
+++ src/Driving/Api/Program.cs
@@ -43,15 +43,18 @@ services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FCG Catalog API v1");
-        c.RoutePrefix = string.Empty;
+
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FCG Catalogs API");
+
+        c.OAuthClientId(appSettings.AuthenticationSettings.Audience);
+        c.OAuthAppName("FCG Catalogs API - Swagger");
+        c.OAuthUsePkce();
     });
+
     app.MapOpenApi();
 }
 
